37199e0c711d13f4f5165f9714c5dafbd80d8af7
Andreas Gasser <andreas.safe@gmail.com>
2018-08-19 21:19:52 +0200

    update application root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import configureStore from './redux/config/configureStore';
+import { addUnhandledPromiseCatcher } from './common/error/errorHandler';
 
 // import css stylesheets
 import './index.css';
 import '../node_modules/@fortawesome/fontawesome-pro/css/all.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { store, persistor } = configureStore();
+
+// add error handler
+addUnhandledPromiseCatcher(store);
+
+class Application extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>           
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById('root'));
 registerServiceWorker();
