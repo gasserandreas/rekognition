464964efe84b809621c03a6b73774f5954953f0c
Andreas Gasser <andreas.safe@gmail.com>
2018-09-20 19:31:40 +0200

    #42 added new text and improved styling for index view

--- src/routes/index/IndexView.jsx
+++ src/routes/index/IndexView.jsx
@@ -7,6 +7,7 @@ import Button from '../../components/FormComponents/Button/Button';
 
 import * as Paths from '../../enums/Paths';
 
+import landing_image from './landing_image.png';
 import './IndexView.css';
 
 class IndexView extends Component {
@@ -60,19 +61,27 @@ class IndexView extends Component {
     return (
       <div className="index">
         <section className="image-container">
-         <div className="show-small">{this.renderTitle()}</div>
-          <p>This application is using the machine learning based <span className="colored">AWS Rekognition</span> feature to detect labels and faces on any picture uploaded from this application.</p>
-          <h1>Key features included</h1>
-          <ul>
-            <li>Label rekognition</li>
-            <li>Face detection</li>
-            <li>Face emotion analysis</li>
-            <li>Face position evaluation</li>
-          </ul>
-          <section className="rekognition-example hide-small">
-            <h1>Example</h1>
-  
-          </section>
+          <div>
+          <div className="show-small">{this.renderTitle()}</div>
+            <p>Analyse image the way you've never done before. Using machine learned base <span className="colored">AWS Rekognition</span> features to detect labels and faces on any uploaded picture. Detect hidden emotions or faces on picture by using the massive compute capacity offered by the Amazon Cloud environment.</p>
+            <img alt="example-analysis" src={landing_image} />
+          </div>
+          <div className="key-feature">
+            <h1>Key Features</h1>
+            <ul>
+              <li>
+                <i className="fas fa-tags" />Label / tags rekognition
+              </li>
+              <li>
+                <i className="fas fa-smile" />
+                Face detection
+              </li>
+              <li>
+                <i className="fas fa-sad-cry" />
+                Face emotion analysis
+              </li>
+            </ul>
+          </div>
         </section>
         <section className="sidebar-container">
           <div className="hide-small">{this.renderTitle()}</div>
