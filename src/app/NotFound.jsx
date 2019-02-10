ed3b28e4455247111818b56531d8e02604cedffe
Andreas Gasser <andreas.safe@gmail.com>
2019-02-10 21:06:19 +0100

    #33 improve NotFound page

--- src/app/NotFound.jsx
+++ src/app/NotFound.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Heading} from 'grommet';
 
-import { Colors } from '../styles';
+import View from '../ui/View';
 
-const StyledNotFound = styled.div`
+import { Colors } from '../styles';
+import * as Paths from '../paths';
+
+const timer = 5;
+
+const StyledNotFound = styled(View)`
   margin-top: 2rem;
 
   h1 {
@@ -20,16 +25,35 @@ const StyledNotFound = styled.div`
   }
 `;
 
-const NotFound = ({ location }) => (
-  <StyledNotFound>
-    <Heading level={1}>
-      <strong>Whhooppss</strong>
-      {' '}
-it seems there is no page for route:
-      <code>{location.pathname}</code>
-    </Heading>
-  </StyledNotFound>
-);
+class NotFound extends Component {
+  timer = null;
+
+  componentWillMount() {
+    setTimeout(() => {
+      const { history } = this.props;
+      history.push(Paths.HOME);
+    }, timer * 1000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    return (
+      <StyledNotFound>
+        <Heading level={1}>
+          <p>
+          <strong>Whhooppss</strong>
+          {' '}
+    it seems there is no page for route:
+          <code>{this.props.location.pathname}</code></p>
+          <p>Forward to home page in: {timer} seconds.</p>
+        </Heading>
+      </StyledNotFound>
+    );
+  }
+}
 
 NotFound.propTypes = {
   location: PropTypes.shape({
