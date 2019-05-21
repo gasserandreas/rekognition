e5258609c50b704c3c642996a85010f5b46a78a4
Andreas Gasser <andreas.gasser@novartis.com>
2019-05-21 18:29:59 +0200

    fix(#121): fixed display bug for detail view image

--- src/ui/async/AsyncImage.jsx
+++ src/ui/async/AsyncImage.jsx
@@ -9,13 +9,14 @@ import LoadingIndicator from './LoadingIndicator';
 export const StyledAsyncImage = styled(Box)`
   justify-content: center;
   transition: all 0.5s ease;
+  ${props => props.wrapperStyles || ''}
   img {
     opacity: ${props => (props.loaded ? '1' : '0')};
     display: ${props => (props.loaded || props.neverHideImg ? 'flex' : 'none')};
   }
 `;
 
-const AsyncImage = ({ onLoad, ...props }) => {
+const AsyncImage = ({ onLoad, wrapperStyles, ...props }) => {
   const [loaded, setLoaded] = useState(false);
 
   const handleOnImageLoad = (e) => {
@@ -28,7 +29,7 @@ const AsyncImage = ({ onLoad, ...props }) => {
   };
 
   return (
-    <StyledAsyncImage loaded={loaded}>
+    <StyledAsyncImage wrapperStyles={wrapperStyles} loaded={loaded}>
       {!loaded && <LoadingIndicator />}
       <Image {...props} onLoad={handleOnImageLoad} />
     </StyledAsyncImage>
@@ -39,11 +40,13 @@ AsyncImage.propTypes = {
   src: PropTypes.string.isRequired,
   neverHideImg: PropTypes.bool,
   onLoad: PropTypes.func,
+  wrapperStyles: PropTypes.string,
 };
 
 AsyncImage.defaultProps = {
   neverHideImg: false,
   onLoad: null,
+  wrapperStyles: null,
 };
 
 export default AsyncImage;
